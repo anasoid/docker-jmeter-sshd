2e506832913a61fa230b32b269e09d72c93a0f5d
Anas OUFDOU <oufdou.anas@gmail.com>
2020-06-08 17:01:28 +0100

    docker jmeter sshd

---
+++ docker/jmeter/jmeter-5.2.1/bin/setenv.bat
@@ -0,0 +1,4 @@
+set PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\Users\admin\AppData\Local\Microsoft\WindowsApps;C:\Java\jdk1.8.0_201\bin
+set HEAP=-Xms1g -Xmx4g -XX:MaxMetaspaceSize=256m
+echo %PATH%
+java -version
